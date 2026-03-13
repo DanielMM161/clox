f3a9a25b3a147c82a2ece622bc45719824d83ce7
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2026-03-13 18:39:48 +0200

    Implement number and identifier token scanning, including keyword recognition.

--- scanner.c
+++ scanner.c
@@ -34,7 +34,12 @@ Token scanToken() {
     if(isAtEnd()) 
         return makeToken(TOKEN_EOF);
 
-    char c =  advance();
+    char c = advance();
+    
+    // scan lexeme start with letter or underscore
+    if(isAlpha(c)) return identifier();
+    // scan lexeme start with digit
+    if(isDigit(c)) return number();
 
     switch(c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -163,4 +168,79 @@ static Token string() {
     // Closing quote
     advance();
     return makeToken(TOKEN_STRING);
+}
+
+static boold isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part
+    if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the '.'
+        advance();
+
+        while (isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || 
+           (c >= 'A' && c <= 'Z') || 
+           (c == '_');
+}
+
+static Token identifier() {
+    while (isAlpha(peek()) || isDigit(peek())) advance();
+
+    return makeToken(TOKEN_IDENTIFIER);
+}
+
+static TokenType identifierType() {
+    switch(scanner.start[0]) {
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f': 
+            if(scanner.current - scanner.start > 1) {
+                switch(scanner.start[1]) {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i':return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't':
+          if (scanner.current - scanner.start > 1) {
+            switch (scanner.start[1]) {
+                    case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);                    
+                }
+            }
+            break;
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+        }
+
+    return TOKEN_IDENTIFIER;
+          }
+
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
+    // the lexeme must be exactly as long as the keyword.
+    // if first letter is 's', the lexeme could still be 'sup' or 'superb', remaining characters must match exactly
+    if(scanner.current - scanner.start == start + length && memcmp(scanner.start + start, rest, length) == 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
 }
